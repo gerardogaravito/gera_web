80d00f40c4113ec567311b466fef02c9b86623f6
Gerardo Garavito <garavitogerardo@gmail.com>
2021-01-19 18:08:41 -0600

    added contact info to the database

--- database/data.js
+++ database/data.js
@@ -8,6 +8,17 @@ const data = {
       month: 'April',
       year: '2000'
     },
+    contact: {
+      email: {
+        gmail: 'garavitogerardo@gmail.com',
+        outlook: 'ggaravito2000@hotmail.com',
+        university: 'gerardo.garavitogrc@uanl.edu.mx'
+      },
+      phone: 8115110723,
+      linkedin: 'https://www.linkedin.com/in/gerardo-garavito-b408811b2/',
+      github: 'https://github.com/gerardogaravito',
+      instagram: 'https://www.instagram.com/garavito666/?hl=es-la'
+    },
   },
 }
 
