25787ea71e18567f4efe316c0b7eb7c5ccb5377d
Gerardo Garavito <garavitogerardo@gmail.com>
2021-01-21 02:36:35 -0600

    made the deployment in main branch

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,6 +8,7 @@ export const Header = () => {
         <Name>Gerardo Garavito García</Name>
         <Profession>Front-End Developer</Profession>
         <Bio>Member of Platzi Master and <br/>learning to be a Software Engineer at UANL.</Bio>
+        <p>Still working on it</p>
       </Info>
     </Container>
   )
