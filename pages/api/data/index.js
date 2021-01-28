4db3209f69de153ea6b88e552b833ed0089b9614
Gerardo Garavito <garavitogerardo@gmail.com>
2021-01-27 22:26:47 -0600

    Trying to use nextjs-cors

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -1,37 +1,53 @@
 import Cors from 'cors'
+import NextCors from 'nextjs-cors'
 
 import data from '@database/data'
 // import initMiddleware from '@database/init-middleware'
 
 // Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-  // origin: ['https://gerardogaravito.vercel.app/','http://localhost:3000/'],
-  origin: ['*']
-})
+// const cors = Cors({
+//   methods: ['GET', 'HEAD'],
+//   // origin: ['https://gerardogaravito.vercel.app/','http://localhost:3000/'],
+//   origin: ['*']
+// })
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+// // Helper method to wait for a middleware to execute before continuing
+// // And to throw an error when an error happens in a middleware
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
 
-      return resolve(result)
-    })
-  })
-}
+//       return resolve(result)
+//     })
+//   })
+// }
 
-async function handler(req, res) {
+// async function handler(req, res) {
 
-  //correr cors
-  await runMiddleware(req, res, cors)
+//   //correr cors
+//   await runMiddleware(req, res, cors)
 
-  res.statusCode = 200
-  res.setHeader('Content-type', 'application/json')
-  res.end(JSON.stringify(data))
-}
+//   res.statusCode = 200
+//   res.setHeader('Content-type', 'application/json')
+//   res.end(JSON.stringify(data))
+// }
 
 export default handler
+
+// ----------------------------- nextjs-cors
+async function handler(req, res) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://www.npmjs.com/package/cors
+  await NextCors(req, res, {
+     // Options
+      methods: ['GET'],
+      origin: '*',
+     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' });
+}
