dba8969c0373e9341e2443fcd9d4e9f926060b27
Gerardo Garavito <garavitogerardo@gmail.com>
2021-01-27 21:09:14 -0600

    changed the cors configuration to the one that Dimaps showed me

--- pages/api/data/index.js
+++ pages/api/data/index.js
@@ -1,22 +1,37 @@
 import Cors from 'cors'
 
 import data from '@database/data'
-import initMiddleware from '@database/init-middleware'
+// import initMiddleware from '@database/init-middleware'
 
-// inicializar el cors middleware
-const cors = initMiddleware(
-  Cors({
-    methods: ['GET', 'POST', 'OPTIONS'],
-    origin: ['https://gerardogaravito.vercel.app/','http://localhost:3000/'],
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+  // origin: ['https://gerardogaravito.vercel.app/','http://localhost:3000/'],
+  origin: ['*']
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
   })
-)
+}
 
-export default handler = async (req, res) => {
+async function handler(req, res) {
 
   //correr cors
-  await cors(req, res)
+  await runMiddleware(req, res, cors)
 
   res.statusCode = 200
   res.setHeader('Content-type', 'application/json')
   res.end(JSON.stringify(data))
 }
+
+export default handler
